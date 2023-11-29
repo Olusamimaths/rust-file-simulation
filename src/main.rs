6d827e5fa39f006035306a2dbd412d3441e5dd4d
olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2023-11-29 02:47:59 +0100

    :fire: File simulation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-#![allow(unused_variables)]
+use rand::prelude::*;
+
+fn one_in(denominator: u32) -> bool {
+    thread_rng().gen_ratio(1, denominator)
+}
 
 #[derive(Debug)]
 struct File {
@@ -20,30 +24,40 @@ impl File {
         f
     }
 
-    fn read(self: &File, save_to: &mut Vec<u8>) -> usize {
+    fn read(self: &File, save_to: &mut Vec<u8>) -> Result<usize, String> {
         let mut tmp = self.data.clone();
         let read_length = tmp.len();
         save_to.reserve(read_length);
         save_to.append(&mut tmp);
-        read_length
+        Ok(read_length)
     }
 }
 
-fn open(f: &mut File) -> bool {
-    true
+fn open(f: File) -> Result<File, String> {
+    if one_in(10_000) {
+        let err_msg = String::from("Permission denied");
+        return Err(err_msg);
+    }
+    Ok(f)
 }
 
-fn close(f: &mut File) -> bool {
-    true
+fn close(f: File) -> Result<File, String> {
+    if one_in(10_000) {
+        let err_msg = String::from("Interrupted by signal!");
+        return Err(err_msg);
+    }
+    Ok(f)
 }
+
 fn main() {
     let f_data: Vec<u8> = vec![114, 117, 115, 116, 33];
     let mut f = File::new_with_data("file.txt", &f_data);
 
     let mut buffer: Vec<u8> = vec![];
-    open(&mut f);
-    let f_length = f.read(&mut buffer);
-    close(&mut f);
+
+    f = open(f).unwrap();
+    let f_length = f.read(&mut buffer).unwrap();
+    f = close(f).unwrap();
 
     let text = String::from_utf8_lossy(&buffer);
     println!("{:?}", f);
