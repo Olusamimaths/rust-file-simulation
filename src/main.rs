96d08effde4d1467be8af7b01c89cd14fb5d50f9
olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2023-12-05 12:48:18 +0100

    :art: Usign enums

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,17 @@ fn one_in(denominator: u32) -> bool {
     thread_rng().gen_ratio(1, denominator)
 }
 
+#[derive(Debug, PartialEq)]
+enum FileState {
+    Open,
+    Closed,
+}
+
 #[derive(Debug)]
 struct File {
     name: String,
     data: Vec<u8>,
+    state: FileState,
 }
 
 impl File {
@@ -15,6 +22,7 @@ impl File {
         File {
             name: String::from(name),
             data: Vec::new(),
+            state: FileState::Closed,
         }
     }
 
@@ -25,6 +33,10 @@ impl File {
     }
 
     fn read(self: &File, save_to: &mut Vec<u8>) -> Result<usize, String> {
+        if self.state != FileState::Open {
+            return Err(String::from("File must be open for reading"));
+        }
+
         let mut tmp = self.data.clone();
         let read_length = tmp.len();
         save_to.reserve(read_length);
@@ -33,32 +45,30 @@ impl File {
     }
 }
 
-fn open(f: File) -> Result<File, String> {
-    if one_in(10_000) {
-        let err_msg = String::from("Permission denied");
-        return Err(err_msg);
-    }
+fn open(mut f: File) -> Result<File, String> {
+    f.state = FileState::Open;
     Ok(f)
 }
 
-fn close(f: File) -> Result<File, String> {
-    if one_in(10_000) {
-        let err_msg = String::from("Interrupted by signal!");
-        return Err(err_msg);
-    }
+fn close(mut f: File) -> Result<File, String> {
+    f.state = FileState::Closed;
     Ok(f)
 }
 
 fn main() {
-    let f_data: Vec<u8> = vec![114, 117, 115, 116, 33];
-    let mut f = File::new_with_data("file.txt", &f_data);
-
+    let mut f = File::new("file.txt");
     let mut buffer: Vec<u8> = vec![];
+
+    if f.read(&mut buffer).is_err() {
+        println!("Error occured");
+    }
+
     f = open(f).unwrap();
     let f_length = f.read(&mut buffer).unwrap();
     f = close(f).unwrap();
 
     let text = String::from_utf8_lossy(&buffer);
+
     println!("{:?}", f);
     println!("{} is {} bytes long", f.name, f_length);
     println!("{}", text);
